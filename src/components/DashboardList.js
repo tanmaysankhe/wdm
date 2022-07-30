44fbb4c5b75fd4112fa337e6b635c18479fb220e
wxv8525 <99681472+wxv8525@users.noreply.github.com>
2022-07-30 16:14:06 -0500

    Dashboard changes

--- src/components/DashboardList.js
+++ src/components/DashboardList.js
@@ -1,46 +1,160 @@
 import React from "react";
-import './css/Login.css'
+import "./css/Login.css";
+import "./css/DashboardList.css";
 
 class DashboardList extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { curtab: "" };
-    }
+  constructor(props) {
+    super(props);
+    this.state = { curtab: "" };
+  }
 
-    items = [
-        { id: 1, name: 'Microsoft' },
-        { id: 2, name: 'Google' },
-        { id: 3, name: 'DELL' }
-    ];
+  items = [
+    { id: 1, name: "Microsoft" },
+    { id: 2, name: "Google" },
+    { id: 3, name: "DELL" },
+  ];
 
-    render() {
-        return (
-            <div>
-                <table>
-                    <tr>
-                        <th>Name</th>
-                        <th>Country</th>
-                        <th></th>
-                        <th></th>
-                    </tr>
-                    {this.items.map((item) => 
-                        
-                            <tr>
-                                <td>{item.id}</td>
-                                <td>{item.name}</td>
-                                <td><button>Edit</button></td>
-                                <td><button>Delete</button></td>
-                            </tr>
-                    
-                    )}
-
-                </table>
-                <ul>
-
-                </ul>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h2>Project Table</h2>
+        <table className="dashboardtable">
+          <tr>
+            <th>Project ID</th>
+            <th>User ID</th>
+            <th>Owner Name</th>
+            <th>Action</th>
+            <th>Cost</th>
+            <th>Date</th>
+            <th>Edit/Delete</th>
+          </tr>
+          <tr>
+            <td>127367827</td>
+            <td>221031476</td>
+            <td>Tanmay</td>
+            <td>Sell</td>
+            <td>12900$</td>
+            <td>07-30-2008</td>
+            <td>
+              <button>Edit</button> <button>Delete</button>
+            </td>
+          </tr>
+          <tr>
+            <td>127367827</td>
+            <td>221031678</td>
+            <td>Zulfiya</td>
+            <td>Buy</td>
+            <td>12900$</td>
+            <td>07-30-2008</td>
+            <td>
+              <button>Edit</button> <button>Delete</button>
+            </td>
+          </tr>
+          <tr>
+            <td>127367827</td>
+            <td>221031893</td>
+            <td>Waseem</td>
+            <td>Sell</td>
+            <td>12900$</td>
+            <td>07-30-2008</td>
+            <td>
+              <button>Edit</button> <button>Delete</button>
+            </td>
+          </tr>
+        </table>
+        <h2>Land Table</h2>
+        <table className="dashboardtable">
+          <tr>
+            <th>Project ID</th>
+            <th>User ID</th>
+            <th>Owner Name</th>
+            <th>Action</th>
+            <th>Cost</th>
+            <th>Date</th>
+            <th>Edit/Delete</th>
+          </tr>
+          <tr>
+            <td>127367827</td>
+            <td>221031893</td>
+            <td>Waseem</td>
+            <td>Sell</td>
+            <td>12900$</td>
+            <td>07-30-2008</td>
+            <td>
+              <button>Edit</button> <button>Delete</button>
+            </td>
+          </tr>
+          <tr>
+            <td>127367827</td>
+            <td>221031893</td>
+            <td>Waseem</td>
+            <td>Sell</td>
+            <td>12900$</td>
+            <td>07-30-2008</td>
+            <td>
+              <button>Edit</button> <button>Delete</button>
+            </td>
+          </tr>
+          <tr>
+            <td>127367827</td>
+            <td>221031893</td>
+            <td>Waseem</td>
+            <td>Sell</td>
+            <td>12900$</td>
+            <td>07-30-2008</td>
+            <td>
+              <button>Edit</button> <button>Delete</button>
+            </td>
+          </tr>
+        </table>
+        <h2>Trails</h2>
+        <table className="dashboardtable">
+          <tr>
+            <th>User ID</th>
+            <th>User Name</th>
+            <th>Outcome</th>
+            <th>Trail Type</th>
+            <th>Project/Land ID</th>
+            <th>Date</th>
+            <th>Edit/Delete</th>
+          </tr>
+          <tr>
+            <td>127367827</td>
+            <td>Waseem</td>
+            <td>Opposite</td>
+            <td>Land</td>
+            <td>127367827</td>
+            <td>07-30-2008</td>
+            <td>
+              <button>Edit</button> <button>Delete</button>
+            </td>
+          </tr>
+          <tr>
+            <td>127367827</td>
+            <td>Waseem</td>
+            <td>Opposite</td>
+            <td>Land</td>
+            <td>127367827</td>
+            <td>07-30-2008</td>
+            <td>
+              <button>Edit</button> <button>Delete</button>
+            </td>
+          </tr>
+          <tr>
+            <td>127367827</td>
+            <td>Waseem</td>
+            <td>Opposite</td>
+            <td>Land</td>
+            <td>127367827</td>
+            <td>07-30-2008</td>
+            <td>
+              <button>Edit</button> <button>Delete</button>
+            </td>
+          </tr>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default DashboardList;
