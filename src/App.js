146622ec38ee97f6b8f944d317ecaa4c97f2e510
Silverlabs Inc <silverlabsinc@Silverlabss-MacBook-Pro.local>
2022-07-30 11:05:04 -0500

    more css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-import About from './components/About';
-import Parent from './components/Parent';
+import logo from "./logo.svg";
+import React from "react";
+import "./App.css";
+import About from "./components/About";
+import Parent from "./components/Parent";
 
 function App() {
   return (
